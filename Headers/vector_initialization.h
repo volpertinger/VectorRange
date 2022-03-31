d2d20c99a9093b842bb79da967eb15994e683ce0
volpertinger <merzlovnik@mail.ru>
2022-04-01 01:51:15 +0300

    code style fix

--- Headers/vector_initialization.h
+++ Headers/vector_initialization.h
@@ -10,12 +10,12 @@
 #define BUFFER_SIZE 20
 
 // формула для получения элемента вектора по двум переменным
-double get_current_element(const int x,const int y);
+double get_current_element(const int x, const int y);
 
 // генерирует вектор векторов размерности size с числом
 // векторов number и выводит в поток output
 // в каждой строке - вектор, через пробелы - его элементы
-int write_vectors(FILE *output, const int size,const int number);
+int write_vectors(FILE *output, const int size, const int number);
 
 // Из потока input читает массив векторов размерности size размера number.
 double **get_vector(FILE *input, const int size, const int number);
